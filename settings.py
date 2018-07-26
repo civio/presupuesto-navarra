0c756c1cd17e7cbf99c1c2dec230194ef3e7cfab
David Cabo <david.cabo@gmail.com>
2018-07-26 16:07:49 +0200

    Modify extra URL settings to latest core version

--- settings.py
+++ settings.py
@@ -139,6 +139,6 @@ MAIN_ENTITY_LEGAL_URL   = 'http://www.navarra.es/home_es/Aviso/avisoLegal.htm'
 
 # We can define additional URLs applicable only to the theme. These will get added
 # to the project URL patterns list.
-EXTRA_URLS = i18n_patterns('presupuesto-navarra.views',
+EXTRA_URLS = (
     url(r'^visita-guiada$', 'guidedvisit', name="guidedvisit"),
 )
