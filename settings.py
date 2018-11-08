a41c364ed8b3682bc2197242d4fd1464093e8839
Eduardo Sebastian <esebastian@altiora.es>
2018-11-08 23:59:54 +0100

    Remove obsolete setting
    civio/presupuesto-management#603

--- settings.py
+++ settings.py
@@ -79,8 +79,6 @@ SHOW_TAX_RECEIPT        = True
 # Show an extra tab with funding breakdown (only applicable to some budgets). Default: False.
 # SHOW_FUNDING_TAB = False
 
-# Adjust inflation in amounts in Overview page. Default: True
-ADJUST_INFLATION_IN_OVERVIEW = False
 
 # Show Subtotals panel in Overview. Default: False
 # SHOW_OVERVIEW_SUBTOTALS = True
