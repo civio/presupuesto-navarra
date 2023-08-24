46fdd9feef2fc27cee35a73c3f42ff20d1907937
David Cabo <david.cabo@gmail.com>
2023-08-24 23:46:13 +0200

    Remove unnecessary imports civio/presupuesto-management#80

--- views/guidedvisit.py
+++ views/guidedvisit.py
@@ -1,9 +1,8 @@
 # -*- coding: UTF-8 -*-
-from coffin.shortcuts import render_to_response
+
 from budget_app.views.helpers import *
 
 def guidedvisit(request, render_callback=None):
-    # Get request context
     c = get_context(request, css_class='body-entities', title='')
 
     return render_to_response('guidedvisit/index.html', c)
