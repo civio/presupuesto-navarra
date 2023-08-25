e5296c117558b5df04d46fd44918f2d062bf5447
David Cabo <david.cabo@gmail.com>
2023-08-25 22:29:18 +0200

    Fix render function name civio/presupuesto-management#80

--- views/guidedvisit.py
+++ views/guidedvisit.py
@@ -5,4 +5,4 @@ from budget_app.views.helpers import *
 def guidedvisit(request, render_callback=None):
     c = get_context(request, css_class='body-entities', title='')
 
-    return render_to_response('guidedvisit/index.html', c)
+    return render_response('guidedvisit/index.html', c)
