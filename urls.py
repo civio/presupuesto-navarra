d9fab470d924adffd4cd120b2bee4341f49b4e5c
David Cabo <david.cabo@gmail.com>
2023-08-28 21:00:20 +0200

    Define URL patterns using callables, not view names, for future Django compatibility civio/presupuesto-management#80

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 
+# We can't import the theme module directly because it has a hyphen in its name. This works well.
+import importlib
+theme_views = importlib.import_module('presupuesto-navarra.views')
+
 # We can define additional URLs applicable only to the theme. These will get added
 # to the project URL patterns list.
 EXTRA_URLS = (
-    url(r'^visita-guiada$', 'guidedvisit', name='guidedvisit'),
-    url(r'^covid$', 'covid', name='covid'),
+    url(r'^visita-guiada$', theme_views.guidedvisit, name='guidedvisit'),
+    url(r'^covid$', theme_views.covid, name='covid'),
 )
